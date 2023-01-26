4c53d48ef9ce2d8c41992504277d6fbebe66d7ad
nmendez4 <nmendez6594@gmail.com>
2023-01-26 09:56:37 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+using System.Collections.Generic;
+
+namespace CatWorx.Badgemaker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> employees = new List<string>();
+            //collect user values until the value is an empty string
+            while (true) {
+            Console.WriteLine("Please enter a name: ");
+            // get a name from the console and assign it to a variable
+            string input = Console.ReadLine() ?? "";
+            // break if the user hits enter w/o typing a name
+            if (input == "") 
+            {
+                break;
+            }
+            employees.Add(input);
+            }
+            for (int i = 0; i <employees.Count; i++)
+            {
+                Console.WriteLine(employees[i]);
+            }
+        }
+    }
+}
